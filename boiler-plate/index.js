049a885d8c6bc827641e91039e47f1c2ec54328f
Hyojeong-Yu <hjeong@lunit.io>
2021-04-17 17:20:14 +0900

    [노드] POST /register API

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,14 @@
 const express = require('express') // express 모듈 가져와서
 const app = express() // express instance 생성
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// client 의 요청을 분석할 수 있도록 함
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://hjeong:gywjd465@boilerplate.ptu1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -8,8 +16,22 @@ mongoose.connect('mongodb+srv://hjeong:gywjd465@boilerplate.ptu1c.mongodb.net/my
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입 시 필요한 정보를 client 에서 가져와서
+  // 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
